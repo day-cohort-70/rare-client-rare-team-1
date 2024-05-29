e7a461586337ce7b96d27634db9b5e9da9154981
William Woodard <williamwoodard98@gmail.com>
2024-05-29 15:27:04 -0500

    finished login ticket, now takes email and password

--- src/Rare.jsx
+++ src/Rare.jsx
@@ -4,10 +4,10 @@ import { NavBar } from "./components/nav/NavBar"
 
 
 export const Rare = () => {
-  const [token, setTokenState] = useState(localStorage.getItem('auth_token'))
+  const [token, setTokenState] = useState(localStorage.getItem('rare_user'))
 
   const setToken = (newToken) => {
-    localStorage.setItem('auth_token', newToken)
+    localStorage.setItem('rare_user', newToken)
     setTokenState(newToken)
   }
 

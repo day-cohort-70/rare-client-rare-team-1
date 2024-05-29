308f5baad47d95fdde953a692ae4bde08fff0fa3
Niki Powell <nhe.powell@gmail.com>
2024-05-29 15:57:19 -0500

    added both admin and user navbar items

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -33,10 +33,35 @@ export const NavBar = ({ token, setToken }) => {
           {
             token
               ?
-              <Link to="/" className="navbar-item">Posts</Link>
+              <Link to="/" className="navbar-item">All Posts</Link>
               :
               ""
           }
+          {
+            token
+              ?
+              <Link to="/" className="navbar-item">My Posts</Link>
+              :
+              ""
+          }
+          {
+            token
+              ?
+              <Link to="/" className="navbar-item">Category Manager</Link>
+              :
+              ""
+          }
+          {
+            token
+              ?
+              <Link to="/" className="navbar-item">Tag Manager</Link>
+              :
+              ""
+          }
+
+
+
+
         </div>
 
         <div className="navbar-end">

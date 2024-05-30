119d5eabd2af4fddc4110c435fd54abb8339672f
William Woodard <williamwoodard98@gmail.com>
2024-05-30 15:51:39 -0500

    can add categories and page refreshes

--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -1,25 +1,34 @@
 import { useEffect, useState } from "react"
 import { getAllCategories } from "../../managers/getAllCategories.jsx"
+import { useNavigate } from "react-router-dom"
 
 
 export const CategoryList = () => {
     const [listOfCategoriesArray, setListOfCategoriesArray] = useState([])
+    const navigate = useNavigate()
 
-    useEffect(() => {
+    const fetchCategories = () => {
         getAllCategories().then(data => {
             setListOfCategoriesArray(data)
-        })
+    })}
+
+    useEffect(() => {
+        fetchCategories()
     }, [])
+
+    const handleClick = () => {
+        navigate("/categorymanager/addCategory", { state: { fetchCategories } } )
+    }
 
     return (
         <div className="categoryListContainer">
             <h2>List of Categories:</h2>
             <article className="categories">
                 {listOfCategoriesArray.map((categoryObj) => {
-                    return <div>{categoryObj.label}</div>
+                    return <div key={categoryObj.id}>{categoryObj.label}</div>
                 })}
             </article>
-
+            <button onClick={handleClick}>Create Category</button>
         </div>
     )
 }

85acfa57118146b428f0e5379ac7d0252ff7b06f
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-05-31 14:52:55 -0400

    post authentication

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { UserPosts } from "../components/UserPosts"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -10,8 +11,9 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/register" element={<Register setToken={setToken} />} />
       <Route element={<Authorized token={token} />}>
         <Route path="/"  />
-        <Route path="/allposts"  />
-        <Route path="myPosts">
+        <Route path="allposts"  />
+        <Route path="myPosts" element={<UserPosts token={token} />} >
+
                     <Route index  />
                     <Route path=":myPostId"  />
                     <Route path=":myPostId/edit"  />
@@ -30,7 +32,6 @@ export const ApplicationViews = ({ token, setToken }) => {
 Elements for the routes
 element={<Home />}
 element={<AllPosts token={token} />}
-element={<MyPosts token={token} />}
 element={<PostDetails />}
 element={<EditPostDetails />}
 element={<AddPost token={token} />}

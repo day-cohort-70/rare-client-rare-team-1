b843ed72206c48cf9a3b076671f7dee95aac8769
Niki Powell <nhe.powell@gmail.com>
2024-06-05 16:06:47 -0500

    added comments to comment list module

--- src/components/post/CommentForm.jsx
+++ src/components/post/CommentForm.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
-import { postComment } from "../../managers/CommentManager.jsx"
+import { getCommentsByPostId, postComment } from "../../managers/CommentManager.jsx"
+import { getOnlyPostByPostId } from "../../managers/PostManager.jsx"
+import { setDate } from "../utils/SetDate.jsx"
 
 
 export const CommentForm = ({token}) => {
     const [comment, setComment] = useState("")
+    const [postData, setPostData] = useState([])
+    const [postComments, setPostComments] = useState([])
 
     const {postId} = useParams()
     const navigate = useNavigate()
 
-    const setDate = () => {
-        const today = new Date()
-        const day = today.getDate()
-        const month = today.getMonth() + 1
-        const year = today.getFullYear()
-        return `${month}/${day}/${year}`
-    }
+
+
+    useEffect(() => {
+        getCommentsByPostId(postId).then(commentArr => {
+            setPostComments(commentArr)
+        }) 
+    },[postId])
+
+    useEffect(() => {
+         getOnlyPostByPostId(postId).then(postObj => {
+            setPostData(postObj)
+         })
+    },[postId])
+
 
     const handleComment = (e) => {
-        e.preventDefault()
+        // e.preventDefault()
 
         const newComment = {
             postId: parseInt(postId),
@@ -28,30 +39,50 @@ export const CommentForm = ({token}) => {
         }
         /* eventually this will need to navigate to the new comments list page for the related post*/
         postComment(newComment).then(() => {
-            navigate(`/`)
+            navigate(`/posts/${postId}/comment`)
         })
     }
 
 
     return (
-        <form key="form-comment" className="container-form-comment">
-            <h2>Leave a Comment</h2>
-            <div className="form-container">
-                <div className="form">
+        <div>
 
-                    <fieldset>
-                        <div className="comment">
-                            <textarea type="text" name="comment" onChange={(event) => {setComment(event.target.value)}} ></textarea>
-                        </div>
-                    </fieldset>
+            <form key="form-comment" className="container-form-comment">
+                <div className="form-container">
+                    <div className="form">
+                        {postData.title}
+                            <fieldset>
+                                <div className="comment">
+                                    <textarea type="text" name="comment" placeholder="Type your comments here.." onChange={(event) => {setComment(event.target.value)}} ></textarea>
+                                </div>
+                            </fieldset>
 
-                    <fieldset>
-                        <div className="container-button">
-                            <button className="button btn-save" onClick={handleComment}>Save</button>
-                        </div>
-                    </fieldset>
+                            <fieldset>
+                                <div className="container-button">
+                                    <button className="button btn-save" onClick={handleComment}>Submit</button>
+                                </div>
+                            </fieldset>
+                    </div>
                 </div>
+            </form>
+
+            <div className="container-post-comments">
+                {postComments
+                    .sort((a, b) => new Date(b.date) - new Date(a.date))
+                    .map(comment => {
+                        return (
+                            <div className="comment" key={comment.id}>
+                                <div>
+                                    <button className="button">EDIT</button>
+                                    <button className="button">DELETE</button>
+                                </div>
+                                <div>{comment?.content}</div>
+                                <div>{comment.author?.username}</div>
+                                <div>{comment?.date}</div>
+                            </div>
+                        )
+                })}
             </div>
-        </form>
+        </div>
     )
 }

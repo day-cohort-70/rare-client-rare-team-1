fdfe415e2bdc5524b0a556cfc4a3662089209c44
Niki Powell <nhe.powell@gmail.com>
2024-06-06 16:01:16 -0500

    added admin approval feature for posts

--- src/managers/PostManager.jsx
+++ src/managers/PostManager.jsx
@@ -1,6 +1,4 @@
 
-/* will need to pass in postId as an argument once this gets linked to posts modules */
-/* will also need to pass in postId as an argument to the function on PostDetails.jsx*/
 export const getPostByPostId = (postId) => {
     return fetch(`http://localhost:8088/posts/${postId}?_expand=user`).then((res) => res.json())
 }

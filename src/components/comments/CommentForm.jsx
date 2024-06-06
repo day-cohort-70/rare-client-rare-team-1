4ccfab80aa9093dd9ce631ff4c67e4cd844bc30b
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-06-06 14:38:29 -0400

    routing from submit comment to comment list fixed

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -29,7 +29,7 @@ export const CommentForm = ({token}) => {
 
 
     const handleComment = (e) => {
-        // e.preventDefault()
+        e.preventDefault()
 
         const newComment = {
             postId: parseInt(postId),

7553432c52c37f217183bfbabdddccdacf3886ba
Leah Sanders <lsanders1814@gmail.com>
2024-06-07 11:16:29 -0500

    all post & new post styling

--- src/components/allPosts/AllPosts.jsx
+++ src/components/allPosts/AllPosts.jsx
@@ -56,25 +56,26 @@ export const AllPosts = () => {
 
 
     return (
-        <div className="container">
+        <div className="container-admin-posts-page">
             <div className="button-new-post">
-                <button className="button" onClick = {()=>{navigate(`/newPost`)}}>New Post</button>
+                <button className="button is-warning m-4" onClick = {()=>{navigate(`/newPost`)}}>New Post</button>
             </div>
 
-            <div className="allposts-container">
+            <div className="container-admin-posts">
                 {sortedPostsByDate.map((post) => {
                     return (
-                        <div className="notification category-item" key={post.id}>
-                            <h3>Title: {post.title}</h3>
-                            <div>Author: {post.user.username}</div>
-                            <div>Category: {post.category.label}</div>
-                            <div>Publication Date: {post.publication_date}</div>
+                        <div className="container-post-info box m-4" style={{ backgroundColor: '#f0f0f0' }} key={post.id}>
+                            <h3 className="container-post-title title has-text-primary">Title: {post.title}</h3>
+                            <div className="container-post-date subtitle">Author: {post.user.username}</div>
+                            <div className="container-post-category is-size-5 has-text-primary">Category: {post.category.label}</div>
+                            <div className="container-post-author is-size-5 has-text-weight-semibold">Publication Date: {post.publication_date}</div>
                             <div className="container-buttons">
-                                <button className="button"><i className="fa-solid fa-gear"></i></button>
-                                <button className="button"><i className="fa-solid fa-trash"></i></button>
-                                <button className="button" onClick={() => {handleClick(post.id)}}>Details</button>
+                                {/* <button className="button is-primary mb-4 ml-4" ><i className="fa-solid fa-gear"></i></button>
+                                <button className="button"><i className="fa-solid fa-trash"></i></button> */}
+                                <button className="button is-primary mb-4 ml-4" onClick={() => {handleClick(post.id)}}>Details</button>
                             </div>
-                            <div className="container-approved">
+                            <div className="columns is-left">
+                                <div className="column is-narrow">
                                 <span>Approved: </span>
                                 <input 
                                     type="checkbox"
@@ -82,6 +83,7 @@ export const AllPosts = () => {
                                     onChange={() => {handleCheckBoxChange(post.id, post.approved)}}
                                 >
                                 </input>
+                                </div>
                             </div>
                         </div>
                     )
